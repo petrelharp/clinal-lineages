3372cd097a4b8cd22fb5c9e7dcb170aa93e6e0cd
peter <petrel.harp@gmail.com>
2015-11-03 17:02:51 -0800

    upper tri

--- notes/unifprod.R
+++ notes/unifprod.R
@@ -1,8 +1,20 @@
 library(Rcpp)
 
+# Discretization of u(a,b) -> \int_a^b (u(a,t) u(t,b) - u(a,b)) dt
+#  so output is y[i,k] = sum( x[i,i:(k-1)]*x[(i+1):k,k] )
+#
+# The key step is to do something like:
+#     1 | x y z
+#   a 2 | . . u
+#     3 | . . v
+#       +------
+#     b   1 2 3
+# --> should  return x*u + y*v
+# the inner product of a partial row with a partial column
+
 unifprod.cpp <- 'NumericMatrix unifprod(NumericMatrix x) {
     int nrx = x.nrow(), ncx = x.ncol();
-    double sum;
+    double sum, xik;
     NumericMatrix z(nrx,nrx);
 
     if (nrx != ncx) stop("Matrix must be square.");
@@ -11,9 +23,9 @@ unifprod.cpp <- 'NumericMatrix unifprod(NumericMatrix x) {
         for (int i = 0; i < nrx; i++)
         for (int k = 0; k < ncx; k++) {
             sum = 0.0;
-            if (i<k)
+            xik = x(i,k);
             for (int j = i; j < k; j++) {
-                sum += x(i , j) * x(j + 1 , k);
+                sum += x(i , j) * x(j + 1 , k) - x(i, k);
             }
             z(i , k) = sum;
         }
@@ -32,21 +44,52 @@ if (FALSE) { ## testing
         stopifnot( NCOL(x) == NROW(x) )
         for (i in seq(1,length.out=nrow(z)-1)) {
             for (k in seq(i+1,ncol(z))) {
-                z[i,k] <- sum(x[i,seq(i,k-1)]*x[seq(i+1,k),k])
+                z[i,k] <- sum(x[i,seq(i,k-1)]*x[seq(i+1,k),k]-x[i,k])
             }
         }
         return(z)
     }
 
-    # must run `R CMD SHLIB unifprod2.c` first
-    dyn.load("unifprod2.so")
-    unifprod2 <- function (x) {
-        .Call("unifprod2",x,nrow(x),ncol(x))
+    unifprod_ut_R <- function (x,ncx) {
+        # same, but for upper triangular ordered x
+        z <- matrix(0,nrow=ncx,ncol=ncx)
+        index <- 0  # where are we in the vector
+        for (j in seq(1,ncx)) {    # column
+            for (i in seq(1,j)) {  # row
+                index <- index + 1
+                pindex <- index + 1      # index of partner in product
+                # z[i,j] <- z[i,j] - (col(z)-row(z))[i,j]*x[index]
+                if ( i<ncx && j<ncx ) {
+                    for (k in j:(ncx-1)) {  # column of product - 1
+                        # A[i,j] contributes to C[i,k] multiplied by A[j+1,k]
+                        #   and the next number to the right is at position +j
+                        pindex <- pindex+k
+                        z[i,k+1] <- z[i,k+1] + x[index]*x[pindex]
+                    }
+                }
+            }
+        }
+        return(z)
     }
 
-    x <- matrix(rnorm(1e4),nrow=100)
-    all( unifprod(x) == unifprod2(x) )
+    # must run `R CMD SHLIB unifprod.c` first
+    dyn.load("unifprod.so")
+    unifprod2 <- function (x) {
+        .Call("unifprod2",as.numeric(x),nrow(x),ncol(x))
+    }
+
+    for (x in list( matrix(1:9,nrow=3), matrix(rnorm(1e4),nrow=100) ) ) {
+        stopifnot( 
+            all.equal( unifprod(x) , unifprod2(x) )
+            && all.equal( unifprod(x) , unifprod_R(x) )
+            && all.equal( unifprod2(x) , unifprod_R(x) )
+        )
+    }
+    library(microbenchmark)
     microbenchmark(unifprod_R(x),times=100)
     microbenchmark(unifprod(x),times=1000)
     microbenchmark(unifprod2(x),times=1000)
+
+    all.equal( unifprod_R(x)[upper.tri(x,diag=TRUE)], unifprod_ut_R(x[upper.tri(x,diag=TRUE)],ncol(x)) )
+
 }
--- notes/unifprod.c
+++ notes/unifprod.c
@@ -3,20 +3,13 @@
 
 /* modified from ./src/main/array.c in R-3.2.2 source */
 
-/*
- dyn.load("unifprod.so")
- unifprod <- function (x,y) { .C("unifprod",x=as.numeric(x),nrx=NROW(x),ncx=NCOL(x),y=as.numeric(y),nry=NROW(y),ncy=NCOL(y),z=matrix(0.0,nrow=NROW(x),ncol=NCOL(y)))[[7]] }
- unifprod(diag(3)*1.0,diag(3)*1.0)
- */
-
 SEXP unifprod2(SEXP x, SEXP Rnrx, SEXP Rncx)
 {
-    /* compute the matrix product (A,B) -> C where
-     *   C[i,j] = A[i]*B[i+1] + A[i+1]*B[i+2] + ... + A[j-1]*B[j]
-     * and so if A is (m x n) and B is (n x o) then C is (m x o) and
+    /* compute the matrix transformation A -> C where
+     *   C[i,j] = A[i,i]*A[i+1,j] + A[i,i+1]*A[i+2,j] + ... + A[i,j-1]*A[j,j] - (j-i) A[i,j]
      *   C[i,i] = 0
      */
-    double sum;
+    double sum, xik;
     int nrx = INTEGER(Rnrx)[0];
     int ncx = INTEGER(Rncx)[0];
     double *X = REAL(x);
@@ -27,10 +20,11 @@ SEXP unifprod2(SEXP x, SEXP Rnrx, SEXP Rncx)
         for (int i = 0; i < nrx; i++)
         for (int k = 0; k < ncx; k++) {
             sum = 0.0;
-            if (i<k)
+            xik = X[i + k * nrx];
             for (int j = i; j < k; j++) {
-                sum += X[i + j * nrx] * X[j + 1 + k * nrx];
+                sum += X[i + j * nrx] * X[j + 1 + k * nrx] - xik;
             }
+            REAL(z)[i + k * nrx] = (double) sum;
             REAL(z)[i + k * nrx] = (double) sum;
         }
     } else { /* zero-extent operations should return zeroes */
