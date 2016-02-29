3b44191fe4f3a75fb9fc2b58851994ab3d024aba
peter <petrel.harp@gmail.com>
2016-02-28 18:02:20 -0800

    minor comments

--- sims/simulation_SIGMA3_Ninds6000_ndemes120_s0.01_dir/compile-pdfs.sh
+++ sims/simulation_SIGMA3_Ninds6000_ndemes120_s0.01_dir/compile-pdfs.sh
@@ -1,2 +1,10 @@
 
 dirs="tau50_dir tau100_dir tau150_dir tau250_dir tau750_dir tau1000_dir"
+
+figs="adjacentBlocksAlongChromAncBConditioning.pdf adjacentBlocksAlongChromNoConditioning.pdf blocksAlongChromAncBConditioning.pdf blocksAlongChromHeatmapAncBConditioning.pdf blocksAlongChromHeatmap.pdf blocksAlongChromNoConditioning.pdf ratioAdjacentBlocksAlongChromAncBConditioning.pdf ratioAdjacentBlocksAlongChromHeatmapAncBConditioning.pdf ratioAdjacentBlocksAlongChromHeatmapNoConditioning.pdf ratioAdjacentBlocksAlongChromNoConditioning.pdf density.pdf ecdf.pdf mean.pdf freqplot.pdf LD.pdf"
+
+for figname in $figs
+do
+    pdfs=$((for dirz in $dirs; do find $dirz -name "*$figname"; done)|tr '\n' ' ')
+    pdfjoin --outfile $figname $pdfs
+done
